ef4b431576bceb79fba980aaafcb9f1e9ad9c89a
Adrian Galindo <asdrian4455@hotmail.com>
2022-02-11 22:43:13 -0500

    first commit

---
+++ src/main/java/zuulproxyauth/authproxy/AuthproxyApplication.java
@@ -0,0 +1,19 @@
+package zuulproxyauth.authproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class AuthproxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthproxyApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/zuulproxyauth/authproxy/AuthproxyApplicationTests.java
@@ -0,0 +1,13 @@
+package zuulproxyauth.authproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthproxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
